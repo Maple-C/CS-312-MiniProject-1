105077fba59878c4efafaeaf989aa8c337947374
Maple-C <cmm2457@nau.edu>
2026-09-14 19:03:01 -0700

    Forgot to say Node_moduels were added in last commit, this now finally gets webpage functioning on localhost 3000

--- index.js
+++ index.js
@@ -2,7 +2,35 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => {
+
+// Bringing the homepage up 
+app.get("/", (req, res) => {
+    res.send("Homepage is now functioning properly");
+});
+
+
+let posts = [];
+
+// for creating new post 
+
+app.post("/create-post", (req, res) => {
+    const { creator} = req.body;
+
+
+    let newPost = {
+        id: Date.now(),
+        creator: creator,
+        createdAt: new Date().toLocaleString()
+    };
+
+    posts.push(newPost); //should add the post then return back home
+   
+    res.redirect("/");
+});
+
+app.listen(port, () => {
     console.log(`The Server is running on port ${port}!!`);
 })
